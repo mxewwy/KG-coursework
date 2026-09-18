20e26bcb69aba89eedd4eb04715f71ac638782b3
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 18:31:20 +0300

    refactor: simplify render header

--- LAB4+/KGlab/Render.h
+++ LAB4+/KGlab/Render.h
@@ -1,24 +1,15 @@
 ﻿#pragma once
 
-#include "MyShaders.h"
-#include "Texture.h"
-#include "ObjLoader.h"
-
 void initRender();
 void Render(double);
 
 extern Shader earthShader;
 extern Texture earthDayTex;
-extern Texture earthNightTex;
 extern Texture earthCloudTex;
-extern ObjModel earthModel;
 extern bool useEarthShader;
 extern Shader backgroundShader;
 extern Texture milkyWayTex;
 extern bool useBackground;
-
-void initBackground();
-void drawBackground();
 
 void initEarthResources();
 void drawEarthWithShaders();
